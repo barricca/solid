c35ff61c4dbc16aaeb2d572ec48855ccb90602b6
barricca <williamsgomess@outlook.com>
2021-12-05 23:55:59 -0300

    S  - Single Responsibility Principle -> Coesão Adicionando no validação e iniciando o O do SOLID

---
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -0,0 +1,22 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public class ReajusteService {
+
+    private final Set<ValidacaoReajuste> validacoes;
+
+    public ReajusteService(Set<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
+
+        var salarioReajustado = funcionario.getSalario().add(aumento);
+        funcionario.atualizarSalario(salarioReajustado);
+    }
+}
---
+++ src/br/com/alura/rh/service/ValidacaoPercentualReajuste.java
@@ -0,0 +1,21 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ValidacaoPercentualReajuste implements ValidacaoReajuste {
+
+    @Override
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+        BigDecimal percentualReajustado = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+
+        if (percentualReajustado.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste não pode ser superior a 40% do salário");
+        }
+    }
+
+}
